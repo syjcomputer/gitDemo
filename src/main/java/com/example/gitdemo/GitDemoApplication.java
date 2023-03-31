4e7779c3228e1fd8601e0329015d619991726407
syjcomputer <sun_computer@163.com>
2023-03-31 17:48:24 +0800

    用户开发基础设施层

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/gitdemo/infrastructure/user/persistence/repository/Po/UserPo.java
@@ -0,0 +1,14 @@
+package com.example.gitdemo.infrastructure.user.persistence.repository.Po;
+
+/**
+ * Demo
+ */
+class UserPo{
+
+    Long id;
+
+    String name;
+
+    String pwd;
+
+}
